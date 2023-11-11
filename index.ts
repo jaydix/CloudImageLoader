088d34ee2194d99120d106e1118f517dfae96a2a
8BitJake <jake8bittaylor@gmail.com>
2023-11-11 16:47:50 -0600

    Revert "i genuinely dont nkow anyomroe"
    This reverts commit ed90c89bbf93994c048cf5f6092c639daf0b1439.

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import WebSocket, { WebSocketServer } from "ws";
+import { WebSocketServer } from "ws";
 import { scratchify } from "./scratchify";
 import jimp from "jimp";
 import puppeteer from "puppeteer";
@@ -7,12 +7,11 @@ import puppeteer from "puppeteer";
   const wss = new WebSocketServer({ port: 8080 });
   const browser = await puppeteer.launch({ headless: "new" });
   const page = await browser.newPage();
-  await page.setViewport({ width: 480, height: 360 });
+  await page.setViewport({ width: 480 * areaMultip, height: 360 * areaMultip });
 
-  wss.on("connection", (ws:WebSocket) => {
-    console.log('HEY');
+  wss.on("connection", (ws) => {
     ws.on("error", console.error);
-    ws.on("message", async (m:any) => {
+    ws.on("message", async (m) => {
       m = m.toString();
       const d = JSON.parse(m);
       switch (d.action) {
